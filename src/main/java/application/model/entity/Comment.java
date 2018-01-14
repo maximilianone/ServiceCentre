8bc2d1330768e18c007c7460744927f3d41cfe09
maximilianone <maximilianone22@gmail.com>
2018-01-14 16:21:22 +0200

    Add files via upload

---
+++ src/main/java/application/model/entity/Comment.java
@@ -0,0 +1,78 @@
+package application.model.entity;
+
+public class Comment {
+    int id;
+    int userID;
+    int orderID;
+    String comment;
+
+    public Comment(int userID, int orderID, String comment) {
+        this.userID = userID;
+        this.orderID = orderID;
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Comment comment1 = (Comment) o;
+
+        if (id != comment1.id) return false;
+        if (userID != comment1.userID) return false;
+        if (orderID != comment1.orderID) return false;
+        return comment.equals(comment1.comment);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + userID;
+        result = 31 * result + orderID;
+        result = 31 * result + comment.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", userID=" + userID +
+                ", orderID=" + orderID +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/application/model/entity/User.java
@@ -0,0 +1,132 @@
+package application.model.entity;
+
+public class User {
+    int id;
+    String firstName;
+    String lastName;
+    String email;
+    String phone;
+    String login;
+    String password;
+    Role role;
+
+    private enum Role {client, admin, main_admin, master}
+
+    public User(String firstName, String lastName, String email, String phone, String login, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.login = login;
+        this.password = password;
+        this.role = Role.client;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role.name();
+    }
+
+    public void setRole(String role) {
+        this.role = Role.valueOf(role);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (!firstName.equals(user.firstName)) return false;
+        if (!lastName.equals(user.lastName)) return false;
+        if (!email.equals(user.email)) return false;
+        if (!phone.equals(user.phone)) return false;
+        if (!login.equals(user.login)) return false;
+        if (!password.equals(user.password)) return false;
+        return role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + phone.hashCode();
+        result = 31 * result + login.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + role.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
