2f6521ee50715febd91489ce767a7a1e945b0bfb
maximilianone <maximilianone22@gmail.com>
2018-01-22 22:14:28 +0200

    Delete RequestMapper.java

--- src/main/java/application/controller/requestMapper/RequestMapper.java
+++
@@ -1,9 +0,0 @@
-package application.controller.requestMapper;
-
-import application.util.constants.RequestParameters;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface RequestMapper<T> extends RequestParameters {
-    T map(HttpServletRequest request);
-}
